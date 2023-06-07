be8ccdbbc989d951a4c9995d01b981eef87a72cb
Marek Polak <markalexpolak@gmail.com>
2023-06-06 23:23:12 -0400

    fixed bug where env file wasnt wroking

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,13 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost', //change this when you do the insomnia
-      dialect: 'mysql',
-      dialectOptions: {
+  : new Sequelize('ecommerce_db', 'mark', 'password', {
+    host: 'localhost', 
+    dialect: 'mysql',
+    dialectOptions: {
         decimalNumbers: true,
       },
     });
+console.log(process.env.DB_USER);
 
 module.exports = sequelize;

1538b403f30a7a4ee31629b58490aaa66b9103b2
Marek Polak <markalexpolak@gmail.com>
2023-06-07 01:03:45 -0400

    minor updates

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,7 +61,7 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
    // update a category by its `id` value
-   Category.update(req.body, {
+   Category.update(req.body.category_name, {
     where: {
       id: req.params.id
     }
